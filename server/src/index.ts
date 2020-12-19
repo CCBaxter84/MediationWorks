46b54616c75e1fff39df50ee2fc06c2db9b12a48
CCBaxter84 <troyceclark@gmail.com>
2020-12-19 15:46:43 -0700

    Got React TypeScript boilerplate working

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,13 +7,8 @@ const PORT = 3000;
 app.use(express.static('./client/public'));
 app.use(express.json());
 
-<<<<<<< HEAD:server/index.js
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/public/index.html'), err => {
-=======
 app.get('/*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'), err => {
->>>>>>> react-server:server/src/index.ts
     if (err) {
       res.status(500).send(err);
     }
