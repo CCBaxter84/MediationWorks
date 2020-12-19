0ab3a7667a6cb6a66ff90c0f2c125d7f8500aab6
CCBaxter84 <troyceclark@gmail.com>
2020-12-19 15:42:16 -0700

    Got express working with typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import path from 'path';
-
-const app = express();
+import express = require('express');
+import { Application, Request, Response } from 'express';
+import * as path from 'path';
+const app: Application = express();
 const PORT = 3000;
 
 app.use(express.static('./client/public'));
 app.use(express.json());
 
-app.get('/*', (req, res) => {
+app.get('/*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'), err => {
     if (err) {
       res.status(500).send(err);
