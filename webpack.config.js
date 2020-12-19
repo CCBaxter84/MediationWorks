4df51c1143155878908dba7fdb7dd1685675d54d
CCBaxter84 <troyceclark@gmail.com>
2020-12-19 12:34:23 -0700

    Successfully created bundle.js from index.ts

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  // Entry point for Webpack
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [ path.resolve(__dirname, 'src') ]
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  }
+}
